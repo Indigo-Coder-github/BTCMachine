0510b08923a8c299b14cbb3cd0af45a0320f855c
현준서 <hjs40111@gmail.com>
2021-08-08 10:15:16 +0000

    주석 첨가

--- core.py
+++ core.py
@@ -8,18 +8,27 @@ class Core():
     def __init__(self, access_key, secret_key, ticker):
         self.access_key = access_key
         self.secret_key = secret_key
+        # api 발급받은 키
         self.ticker = ticker
+        # 기준통화-암호화폐
         self.account = pyupbit.Upbit(access_key, secret_key)
+        # 계정 정보를 저장
 
     def get_target_price(self):
         df = pyupbit.get_ohlcv(self.ticker)
 
         yesterday = df.iloc[-2]
+        # -1이 마지막행==당일을 불러오므로 -2==어제
         today_open = yesterday['close']
+        # 금일 시가는 전일 종가로 잡음
         yesterday_high = yesterday['high']
+        # 전일 고가
         yesterday_low = yesterday['low']
+        # 전일 저가
         target_price = today_open + (yesterday_high - yesterday_low) * 0.5
+        # 매수가 = 시가 + 변동폭의 절반값
         return target_price
+        # 매수가 반환
 
     def buy_crypto_currency(self):
         # krw = self.account.get_balance('KRW')
@@ -33,7 +42,7 @@ class Core():
         self.account.sell.market_order(self.ticker, unit)
 
 
-now = datetime.datetime.now()
+now = datetime.datetime.now()  # 현재 시각
 mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
 core = Core(None, None, 'KRW-XRP')
 
