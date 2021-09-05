563234797cf2479be985a43f3a2c0cbf3d1b1fdc
현준서 <hjs40111@gmail.com>
2021-09-05 11:37:57 +0000

    MDD, 수수료 구현 필요

--- backtesting.py
+++ backtesting.py
@@ -1,7 +1,7 @@
 import pyupbit
 import numpy as np
 
-df = pyupbit.get_ohlcv("KRW-BTC", interval="day", count=7)
+df = pyupbit.get_ohlcv("KRW-BCHA", count=120, interval="day")
 # KRW-BTC 일봉 데이터
 df['range'] = (df['high'] - df['low']) * 0.5
 # 마지막 열이 자동으로 추가됨
@@ -11,5 +11,5 @@ df['ror'] = np.where(df['high'] > df['target'],  df['close']/df['target'], 1)
 # 각 행마다 고가가 목표가를 넘었으면 수익률인 종가와 목표가의 나눈 값을, 아닌 경우 매수하지 않았으므로 수익률은 1
 ror = df['ror'].cumprod()[-2]
 # 기간수익률은 모든 수익률의 곱과 동일
-print(df.tail())
+print(df)
 print(ror)
